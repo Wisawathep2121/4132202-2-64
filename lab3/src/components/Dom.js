ec146fb81753c924a15a0b95797d87433a2bf293
Wisawathep2121 <93894181+Wisawathep2121@users.noreply.github.com>
2022-03-09 13:41:11 +0700

    Add files via upload

---
+++ lab3/src/components/Dom.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+const age = 20;
+
+const dom = (
+    <>
+      <h1>Hello React !!!</h1>
+      <p>My name is Apinan, {age} year old.</p>
+    </>
+);
+
+function Dom(){
+    return dom;
+}
+
+export default Dom
